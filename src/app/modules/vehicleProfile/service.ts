a3c8385c99e9b4416c1c198619e1e278bea50857
md-mobarak <mdmobarakhossen782@gmail.com>
2024-01-29 15:07:28 +0600

    completed crud opearation without testing api

---
+++ src/app/modules/vehicleProfile/service.ts
@@ -0,0 +1,55 @@
+import { PrismaClient } from "@prisma/client";
+
+
+
+const prisma = new PrismaClient()
+
+const createVehicleService =async (payload:any)=>{
+        const result = await prisma.vehicleProfile.create({
+            data: payload
+        })
+     return result
+}
+
+const getAllVehicleService = async (payload:any)=>{
+    const {filters,skip,size,sortBy,sortOrder}=payload
+    const result = await prisma.vehicleProfile.findMany({
+        where:filters,
+        skip,
+        take:Number(size),
+        orderBy:{
+            [sortBy]:sortOrder
+        }
+    })
+    return result
+}
+
+
+
+const updateVehicleProfileService = async (data: any, id: string) => {
+    const result = await prisma.vehicleProfile.update({
+      where: {
+        id: id,
+      },
+      data,
+    });
+    return result;
+  };
+
+  const DeletevehicleProfileService = async (id: string) => {
+    const result = await prisma.vehicleProfile.delete({
+      where: {
+        id,
+      },
+    });
+    return result;
+  };
+ 
+
+
+export const vehicleProfileService ={
+    createVehicleService,
+    getAllVehicleService,
+    updateVehicleProfileService,
+    DeletevehicleProfileService
+}
